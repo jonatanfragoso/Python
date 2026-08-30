7feead2c5bc95c3ffcf540465259ea06b6189265
Jonatan Fragoso <jonatanfragoso@gmail.com>
2026-08-30 13:38:48 -0500

    primeiros programas, operadores logicos e variaveis.

---
+++ dia_01/02_calculadora.py
@@ -0,0 +1,23 @@
+# %%
+# SOMA
+print("1 + 1 = ", 1 + 1)
+
+#%%
+# SUBTRAÇÃO
+print("1 - 1 = ", 1 - 1)
+
+#%%
+# MULTIPLICAÇÃO
+print("2 * 2 = ", 2 * 2)
+
+#%%
+# DIVISÃO
+print("10 / 2 = ", 10 / 2)
+
+#%%
+#DIVISÃO INTEIRA
+print("10 // 3 = ", 10 // 3)
+
+#%%
+#RESTO DA DIVISÃO
+print("10 % 4 = ", 10 % 4)
---
+++ dia_01/03_variaveis.py
@@ -0,0 +1,19 @@
+# %%
+nome = "Jonatan"
+print(nome)
+
+# %%
+#TODOS OS TIPOS DE VARIÁVEIS
+idade = 30 #int
+print(idade)
+
+altura = 1.75 #float
+print(altura)
+
+e_estudante = True #bool
+print(e_estudante)
+
+idade2: int = 25.2
+print(idade2)
+
+# %%
