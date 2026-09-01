f25e41dd1d0ab56cbf736ac1409da112eb63f8ff
Jonatan Fragoso <jonatanfragoso@gmail.com>
2026-09-01 14:51:35 -0500

    consultado dados de uma lista com slice

---
+++ dia_03/listas.py
@@ -0,0 +1,13 @@
+# %%
+
+idades = [24, 56, 76, 23, 44, 22, 34, 49]
+print(idades)
+print(idades[2])
+print(sum(idades)/len(idades))
+
+#fatiar elementos
+print(idades[0:3])
+print(idades[-2:])
+
+
+# %%
