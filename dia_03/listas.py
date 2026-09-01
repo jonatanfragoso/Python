aac1d134af5e3d4490f519ba73ed89b3364e8314
Jonatan Fragoso <jonatanfragoso@gmail.com>
2026-09-01 15:08:48 -0500

    brincando com listas.

--- dia_03/listas.py
+++ dia_03/listas.py
@@ -9,5 +9,19 @@ print(sum(idades)/len(idades))
 print(idades[0:3])
 print(idades[-2:])
 
+print(idades[::-1]) #terceira posição é o salto
+print(idades[::2])
+print(idades[::-2])
 
+# %%
+#NAVEGAR EM UMA LISTA
+lista = [1,2,3,3,2,1,1,1,1,1,5,6,7,7,6,5]
+numero = int(input("Entre com um número: "))
+
+contador = 0
+for i in lista:
+    if i == numero:
+        contador += 1
+
+print(f"Quantidade de {numero}: {contador}")
 # %%
